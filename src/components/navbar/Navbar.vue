aba9a1d8aeacf98edda1373800f2c1e6c8793168
santosh <lgsandy543@gmail.com>
2020-05-18 10:13:43 +0530

    Basic Setup is completed

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,102 @@
+<template>
+  <nav>
+    <v-app-bar color="primary" dark app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-light">AAE</span>
+        <span>IdeaPro</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon left>expand_more</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list flat>
+          <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            router
+            :to="link.route"
+            active-class="border"
+            link
+          >
+            <v-list-item-title>{{link.text}}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <router-link tag="span" to="/login">
+        <v-btn text>
+          <span>Exit</span>
+          <v-icon right>exit_to_app</v-icon>
+        </v-btn>
+      </router-link>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" dark app class="pink lighten-2" :mini-variant.sync="mini">
+      <!-- <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/img1.png" alt />
+          </v-avatar>
+          <p class="white--text subheading mt-1 text-center">Username</p>
+        </v-flex>
+        <v-flex class="mt-4 mb-4">
+          <Popup />
+        </v-flex>
+      </v-layout>-->
+
+      <v-list flat>
+        <v-list-item class="px-2" link>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-title>John Leider</v-list-item-title>
+
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+        <v-divider />
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+          active-class="border"
+        >
+          <v-list-item-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{link.text}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider />
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    links: [
+      { icon: "dashboard", text: "Dashboard", route: "/dashboard" },
+      { icon: "folder", text: "My Project", route: "/projects" },
+      { icon: "person", text: "Team", route: "/team" }
+    ],
+    mini: true
+  }),
+  components: {
+    // Popup
+  }
+};
+</script>
+<style scoped>
+.border {
+  border-left: 4px solid #0ba518;
+}
+</style>
