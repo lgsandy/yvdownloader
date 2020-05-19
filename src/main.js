a958caea031dfd56f67d18b20de9847f2671bf77
Krishnakant Sharma <krishnakant.sharma@semasoft.co.in>
2020-05-19 19:35:10 +0530

    all changes

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import SocialSharing from "vue-social-sharing";
+import VueAnalytics from 'vue-analytics'
 Vue.use(SocialSharing);
-
+Vue.use(VueAnalytics, {
+  id: 'G-BWY3SNM33X'
+})
 Vue.config.productionTip = false;
 
 new Vue({
@@ -15,3 +18,5 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+
